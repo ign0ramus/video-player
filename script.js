8c748c9aea7b92405a4f4e0289e45e47e1dd1209
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-09-03 14:21:43 +0300

    add play and pause

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volume = document.getElementById('volume');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('.time-duration');
+const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
+function showPlayIcon() {
+	playBtn.classList.replace('fa-pause', 'fa-play');
+}
 
+function togglePlay() {
+	if (video.paused) {
+		video.play();
+		playBtn.classList.replace('fa-play', 'fa-pause');
+	} else {
+		video.pause();
+		showPlayIcon();
+	}
+}
 
-
-// Progress Bar ---------------------------------- //
-
-
+// Progress Bar ------------------------------ //
 
 // Volume Controls --------------------------- //
 
+// Change Playback Speed --------------------- //
 
+// Fullscreen -------------------------------- //
 
-// Change Playback Speed -------------------- //
-
-
-
-// Fullscreen ------------------------------- //
-
-
+// Event listeners --------------------------- //
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('ended', showPlayIcon);
