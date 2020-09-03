dfc49e680a04c69b519a58588d9c95045f23d4d7
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-09-03 14:49:21 +0300

    add volume bar

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const video = document.querySelector('video');
 const progressRange = document.querySelector('.progress-range');
 const progressBar = document.querySelector('.progress-bar');
 const playBtn = document.getElementById('play-btn');
-const volume = document.getElementById('volume');
+const volumeIcon = document.getElementById('volume');
 const volumeRange = document.querySelector('.volume-range');
 const volumeBar = document.querySelector('.volume-bar');
 const currentTime = document.querySelector('.time-elapsed');
@@ -46,6 +46,45 @@ function setProgress(e) {
 }
 
 // Volume Controls --------------------------- //
+let lastVolume = 1;
+
+function changeVolumeIcon(volume) {
+	volumeIcon.className = '';
+	if (volume > 0.7) {
+		volumeIcon.classList.add('fas', 'fa-volume-up');
+	} else if (volume < 0.7 && volume > 0) {
+		volumeIcon.classList.add('fas', 'fa-volume-down');
+	} else if (volume === 0) {
+		volumeIcon.classList.add('fas', 'fa-volume-mute');
+	}
+}
+
+function changeVolume(e) {
+	let volume = e.offsetX / volumeRange.offsetWidth;
+	if (volume < 0.1) {
+		volume = 0;
+	} else if (volume > 0.9) {
+		volume = 1;
+	}
+
+	volumeBar.style.width = `${volume * 100}%`;
+	video.volume = volume;
+	changeVolumeIcon(volume);
+	lastVolume = volume;
+}
+
+function toggleMute() {
+	if (video.volume) {
+		lastVolume = video.volume;
+		video.volume = 0;
+		volumeBar.style.width = 0;
+		changeVolumeIcon(0);
+	} else {
+		video.volume = lastVolume;
+		volumeBar.style.width = `${lastVolume * 100}%`;
+		changeVolumeIcon(lastVolume);
+	}
+}
 
 // Change Playback Speed --------------------- //
 
@@ -58,3 +97,5 @@ video.addEventListener('ended', showPlayIcon);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
